1048f0166f7550d01dec1c2b4b885f92ae6b0f83
romanunr02 <romanunr02@gmail.com>
2022-02-07 22:27:53 +0200

    added som previously written code

--- Age verification/Age verification/Age_verification.py
+++ Age verification/Age verification/Age_verification.py
@@ -1,1 +1,21 @@
+def printWelcomeMsg():
+    msg = "Hi im new"
+    print(msg)
 
+
+printWelcomeMsg()
+
+
+def verifyAge():
+    age = int(input("write your age:"))
+    if age < 18:
+        print("minor")
+    else:
+        if age > 18:
+            print("adult")
+        else:
+            print("barely adult")
+
+
+for x in range(4):
+    verifyAge()
