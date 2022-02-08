71fb33db9d50111a5671075b06ac50aaaa299d06
romanunr02 <romanunr02@gmail.com>
2022-02-08 22:25:07 +0200

    added another ex

--- Cuvinte1/Cuvinte1/Cuvinte1.py
+++ Cuvinte1/Cuvinte1/Cuvinte1.py
@@ -1,1 +1,22 @@
 
+def isVocal(charr):
+    for c in ['a', 'e', 'i', 'o', 'u']:
+        if c == charr:
+            return True
+    return False
+
+
+def hasStringConsonants(text):
+    for charr in text:
+        if charr == ' ':
+            continue
+        if isVocal(charr) == False:
+            return True
+    return False
+
+
+textArr = input().split(" ")
+
+for text in textArr:
+    if hasStringConsonants(text) == False:
+        print(text)
